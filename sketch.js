1476ed97c75c5372928f9042380d4b9efdd20fe8
MeMayank07 <baidmayank07@gmail.com>
2021-04-24 23:19:28 +0530

    correction

--- sketch.js
+++ sketch.js
@@ -17,10 +17,10 @@ function draw(){
     bullet.velocityX=0;
     var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
     if(damage>10){
-      wall.shapeColor=color(255,0,0);
+      bullet.shapeColor=color(255,0,0);
     }
     if(damage<10){
-      wall.shapeColor=color(0,255,0);
+      bullet.shapeColor=color(0,255,0);
     }
   }
     drawSprites();
